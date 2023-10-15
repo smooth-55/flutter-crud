ea6c05bf0ad1ea6372403201612859490c1f643c
Ashish <dhakalashish18@gmail.com>
2023-10-15 20:39:05 +0545

    chore: todo api services

---
+++ lib/services/todo.dart
@@ -0,0 +1,18 @@
+import 'package:crud/config/api.dart';
+import 'package:dio/src/response.dart';
+
+final base_url = "/users/todos";
+Future<Response> getTodo() async {
+  var res = await dio.get(base_url);
+  return res;
+}
+
+Future<Response> createTodo(Object? val) async {
+  var res = await dio.post(base_url, data: val);
+  return res;
+}
+
+Future<Response> updateTodo(int id, Object? val) async {
+  var res = await dio.put("${base_url}/${id}", data: val);
+  return res;
+}
