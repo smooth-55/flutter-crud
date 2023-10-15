b290d78a984cfd87cdbae238837c14924952fa4e
Ashish <dhakalashish18@gmail.com>
2023-10-15 12:50:36 +0545

    feat: dio initialize

---
+++ lib/config/api.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+late final dio = Dio(); // With default `Options`.
+const BaseUrl  = 'https://jsonplaceholder.typicode.com/';
+void configureDio() {
+  print("initializing dio");
+  // Set default configs
+  dio.options.baseUrl = BaseUrl;
+  dio.options.connectTimeout = Duration(seconds: 5);
+  dio.options.receiveTimeout = Duration(seconds: 3);
+}
+
