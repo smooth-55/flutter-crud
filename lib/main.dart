5e834e9ccfd8988b77c7e88c5e232adcf02798ab
Ashish <dhakalashish18@gmail.com>
2023-10-15 12:55:02 +0545

    feat: configure dio + theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:crud/pages/create.dart';
+import 'package:crud/config/api.dart';
+import 'package:crud/pages/create_todo.dart';
 import 'package:crud/pages/list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  configureDio();
+
   runApp(
     MaterialApp(
       // title: 'Named Routes Demo',
@@ -12,6 +15,8 @@ void main() {
         '/create-todo': (context) => const CreateTodoForm(),
       },
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.grey),
     ),
   );
 }
+
