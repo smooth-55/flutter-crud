ec9b0f553e9ff34ca96c166cecbaf2277133a0ba
Ashish <dhakalashish18@gmail.com>
2023-10-15 12:56:34 +0545

    feat: fetch todos with dio

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -1,4 +1,6 @@
 import 'package:crud/components/tile.dart';
+import 'package:crud/config/api.dart';
+import 'package:crud/models/todo.dart';
 import 'package:flutter/material.dart';
 
 class TodoList extends StatefulWidget {
@@ -9,25 +11,69 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  List<Todo>? todos;
+
+  _getData() async {
+    try {
+      String url = "/todos";
+      final res = await dio.get(url);
+      if (res.statusCode == 200) {
+        List<dynamic> resp = res.data;
+        List<Todo> todoList = resp.map((data) => Todo.fromJson(data)).toList();
+        todos = todoList;
+        _isLoading = false;
+        setState(() {});
+      }
+    } catch (e) {
+      _isLoading = false;
+      debugPrint(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todos"),
+        // title: Text("Todos", style: TextStyle(color: Color.fromRGBO(189, 219, 208, 1)),),
+        title: Text("Todos", style: TextStyle(color: Colors.white),),
         elevation: 0,
+        backgroundColor: Color.fromRGBO(81, 75, 35, 1),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: 10,
-            itemBuilder: (context, index){
-          return TodoTile();
-        }),
-      ),
-        floatingActionButton: FloatingActionButton(onPressed: (){
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                  itemCount: todos?.length,
+                  itemBuilder: (context, index) {
+                    if (!_isLoading) {
+                      return  TodoTile(
+                        id: todos?[index].id,
+                        title: todos?[index].title,
+                        status: todos?[index].completed,
+                      );
+                    }else{
+                      return Container();
+                    }
+
+                  }),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
           Navigator.pushNamed(context, '/create-todo');
-        }, child: Icon(Icons.add),),
+        },
+        child: Icon(Icons.add, color: Colors.white,),
+      ),
     );
   }
 }
